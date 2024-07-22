079d93d041a4037292db24b6fd32bd5450f1f818
mod-construction <moradi.yashar@gmail.com>
2024-07-22 12:39:55 +0000

    Update from https://github.com/mod-construction/mod-dlm/commit/e6d5346ac0d585aa88d987e48578ad3451662fbb

---
+++ src/test/java/construction/mod/dlm/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package construction.mod.dlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
